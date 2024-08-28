4acfc2c4160474d805e0877e6081f90761a57035
Samuel Cousin <86833137+makanakidev@users.noreply.github.com>
2024-08-29 00:51:30 +0100

    Update initiate_calls_to_dart_in_bg.dart

--- lib/initiate_calls_to_dart_in_bg.dart
+++ lib/initiate_calls_to_dart_in_bg.dart
@@ -4,27 +4,26 @@ import 'package:flutter/services.dart';
 import 'package:initiate_calls_to_dart_in_bg/callbackDispatcher.dart';
 
 class InitiateCalls {
-
   static const MethodChannel _channel = const MethodChannel('main_channel');
 
   static Future<void> initialize() async {
     final callback = PluginUtilities.getCallbackHandle(callbackDispatcher);
-    if(callback != null) {
+    if (callback != null) {
       print('Initializing callbackDispatcher...');
-    await _channel.invokeMethod('initialize',
-        <dynamic>[callback.toRawHandle()]);
+      await _channel
+          .invokeMethod('initialize', <dynamic>[callback.toRawHandle()]);
     } else
       print('Invalid callbackDispatcher!');
   }
 
-  static void run(void Function(String s) callback) async {
+  static void run(void Function(String s, List<String> p) callback,
+      [List<String> params = []]) async {
     final action = PluginUtilities.getCallbackHandle(callback);
-    if(action != null) {
+    if (action != null) {
       print('Sending run callback to native environment...');
-    final List<dynamic> args = <dynamic>[action.toRawHandle()];
-    await _channel.invokeMethod('run', args);
+      final List<dynamic> args = <dynamic>[action.toRawHandle(), params];
+      await _channel.invokeMethod('run', args);
     } else
       print('run callback is invalid!');
   }
-
 }
